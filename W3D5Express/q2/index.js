cea72697ba9b92f83b177ae06568b7068e5f5740
Natnael4 <87682063+Natnael4@users.noreply.github.com>
2021-08-07 10:14:25 -0500

    Update index.js

--- W3D5Express/q2/index.js
+++ W3D5Express/q2/index.js
@@ -3,7 +3,7 @@ const app = express();
 app.use(express.urlencoded({ extended: false }));
 
 app.use('/', (req, res) => {
-    res.send("<form action=result method=post><label>Name: <input name=name type=text></label><label>Age<input name=age type=number></label><button type=submit>Submit Qurey</button></form>");
+    res.send("<form action=result method=post><label>Name: <input name=name type=text></label><label>Age<input name=age type=number></label><button type=submit>Submit Query</button></form>");
 });
 
 
